d3ff2e9655a9985cfd7ad3f084ba854489b4998f
alonns04 <claudiogabrielalonso@gmail.com>
2023-08-24 15:14:40 -0300

    ...

--- script.js
+++ script.js
@@ -1,23 +1,16 @@
-document.getElementByClass("recargar").addEventListener("click", function() {
+document.getElementsByClassName("recargar").addEventListener("click", function() {
     location.reload();
 });
 
 
 
 
-
-// Obtén el elemento HTML donde deseas mostrar el precio
-const precioElement = document.getElementById("precio-bitcoin");
-
-// URL de la API de Binance para el precio de BTCUSDT
 const apiUrl = "https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT";
 
-// Realiza la solicitud a la API de Binance
 fetch(apiUrl)
   .then(response => response.json())
   .then(data => {
-    const precioBitcoin = parseFloat(data.price);
-    precioElement.textContent = precioBitcoin.toFixed(2); // Muestra el precio con 2 decimales
+    console.log(data);
   })
   .catch(error => {
     console.error("Error al obtener el precio de Bitcoin:", error);
