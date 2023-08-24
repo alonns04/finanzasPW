cce32558db984f6b2e1ee6a4b87d47a476deea3e
alonns04 <claudiogabrielalonso@gmail.com>
2023-08-24 18:42:10 -0300

    CRIPTOS

--- script.js
+++ script.js
@@ -43,9 +43,42 @@ function obtenerPrecioBNB() {
             bnbE.textContent = precioBNB.toFixed(2);
         })
         .catch(error => {
-            console.error("Error al obtener el precio de Ethereum:", error);
+            console.error("Error al obtener el precio de BNB:", error);
         });
 }
+
+const usdtarsE = document.getElementById("usdtars")
+
+function obtenerPrecioUSDTARS() {
+    const apiUrl = "https://api.binance.com/api/v3/ticker/price?symbol=USDTARS";
+    
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            const precioUSDTARS = parseFloat(data.price);
+            usdtarsE.textContent = precioUSDTARS.toFixed(2);
+        })
+        .catch(error => {
+            console.error("Error al obtener el precio de USDT - ARS:", error);
+        });
+}
+
+const solE = document.getElementById("sol")
+
+function obtenerPrecioSOL() {
+    const apiUrl = "https://api.binance.com/api/v3/ticker/price?symbol=SOLUSDT";
+    
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            const precioSOL = parseFloat(data.price);
+            solE.textContent = precioSOL.toFixed(2);
+        })
+        .catch(error => {
+            console.error("Error al obtener el precio de SOL", error);
+        });
+}
+
 
 
 
@@ -53,6 +86,10 @@ function obtenerPrecioBNB() {
 obtenerPrecioEthereum();
 obtenerPrecioBitcoin();
 obtenerPrecioBNB();
+obtenerPrecioSOL();
+obtenerPrecioUSDTARS();
+setInterval(obtenerPrecioSOL, 4000);
+setInterval(obtenerPrecioUSDTARS, 4000);
 setInterval(obtenerPrecioBNB, 1500);
 setInterval(obtenerPrecioEthereum, 1500);
 setInterval(obtenerPrecioBitcoin, 1500);
