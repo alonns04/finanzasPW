5cd29cf36b38f00009e1c7758fdfd6fdd017befe
alonns04 <claudiogabrielalonso@gmail.com>
2023-08-24 15:38:55 -0300

    preciobtc hecho

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
-document.getElementsByClassName("recargar").addEventListener("click", function() {
-    location.reload();
-});
+// tu-archivo-js.js
 
+const precioElement = document.getElementById("pb");
 
+function obtenerPrecioBitcoin() {
+    const apiUrl = "https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT";
+    
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            const precioBitcoin = parseFloat(data.price);
+            precioElement.textContent = precioBitcoin.toFixed(2);
+        })
+        .catch(error => {
+            console.error("Error al obtener el precio de Bitcoin:", error);
+        });
+}
 
+// Obtener el precio inicial
+obtenerPrecioBitcoin();
 
-const apiUrl = "https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT";
-
-fetch(apiUrl)
-  .then(response => response.json())
-  .then(data => {
-    console.log(data);
-  })
-  .catch(error => {
-    console.error("Error al obtener el precio de Bitcoin:", error);
-  });
+// Actualizar el precio cada 5 segundos (5000 milisegundos)
+setInterval(obtenerPrecioBitcoin, 1500);
