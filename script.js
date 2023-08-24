015ecf2727738838e82e5459ae5325e01fc4b6e3
alonns04 <claudiogabrielalonso@gmail.com>
2023-08-24 18:08:00 -0300

    precios actualizados

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // tu-archivo-js.js
 
-const precioElement = document.getElementById("pb");
+const bitcoinE = document.getElementById("btn");
 
 function obtenerPrecioBitcoin() {
     const apiUrl = "https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT";
@@ -9,26 +9,50 @@ function obtenerPrecioBitcoin() {
         .then(response => response.json())
         .then(data => {
             const precioBitcoin = parseFloat(data.price);
-            precioElement.textContent = precioBitcoin.toFixed(2);
+            bitcoinE.textContent = precioBitcoin.toFixed(2);
         })
         .catch(error => {
             console.error("Error al obtener el precio de Bitcoin:", error);
         });
 }
+const ethereumE = document.getElementById("eth")
 
-// Obtener el precio inicial
+function obtenerPrecioEthereum() {
+    const apiUrl = "https://api.binance.com/api/v3/ticker/price?symbol=ETHUSDT";
+    
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            const precioEthereum = parseFloat(data.price);
+            ethereumE.textContent = precioEthereum.toFixed(2);
+        })
+        .catch(error => {
+            console.error("Error al obtener el precio de Ethereum:", error);
+        });
+}
+
+const bnbE = document.getElementById("bnb")
+
+function obtenerPrecioBNB() {
+    const apiUrl = "https://api.binance.com/api/v3/ticker/price?symbol=BNBUSDT";
+    
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            const precioBNB = parseFloat(data.price);
+            bnbE.textContent = precioBNB.toFixed(2);
+        })
+        .catch(error => {
+            console.error("Error al obtener el precio de Ethereum:", error);
+        });
+}
+
+
+
+
+obtenerPrecioEthereum();
 obtenerPrecioBitcoin();
-
-// Actualizar el precio cada 5 segundos (5000 milisegundos)
+obtenerPrecioBNB();
+setInterval(obtenerPrecioBNB, 1500);
+setInterval(obtenerPrecioEthereum, 1500);
 setInterval(obtenerPrecioBitcoin, 1500);
-
-const apiUrl = "https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT";
-
-fetch(apiUrl)
-  .then(response => response.json())
-  .then(data => {
-    console.log(data);
-  })
-  .catch(error => {
-    console.error("Error al obtener el precio de Bitcoin:", error);
-  });
